6e88e27ebe466079c771a0bf7c62d58b609c3c53
PRADEEP  A <pradeepashok1122@gmail.com>
2023-01-03 23:38:56 +0530

    test

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ app.post('/signup', function (req, res) {
   var newUser = new User({
     name: req.body.name,
     email: req.body.email,
-    username: req.body.username.replaceAll(' ',''),
+    username: req.body.username.replaceAll(' ','_'),
     branch: req.body.branch,
     year: req.body.year,
     password: req.body.password,
